dc313b6be50f644144a93dcbb35308e72e30db8d
Mirza Krak <mirza.krak@hostmobility.com>
2013-07-16 06:56:41 +0000

    kernel: fixed failing build.

--- arch/arm/mach-tegra/board-colibri_t20.c
+++ arch/arm/mach-tegra/board-colibri_t20.c
@@ -592,7 +592,8 @@ static struct mxc_mma845x_platform_data mma845x_data = {
 /* GEN1_I2C: I2C_SDA/SCL on SODIMM pin 194/196 (e.g. RTC on carrier board) */
 static struct i2c_board_info colibri_t20_i2c_bus1_board_info[] __initdata = {
 //#ifdef CONFIG_SENSORS_L3G4200D
-#if 0	{
+#if 0	
+{
         I2C_BOARD_INFO(L3G4200D_NAME, 0x68),
         .platform_data = &colibri_gyro_pdata,
         .irq = TEGRA_GPIO_TO_IRQ(L3G4200D_DRDY_GPIO),
