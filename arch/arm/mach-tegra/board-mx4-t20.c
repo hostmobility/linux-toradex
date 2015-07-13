67b4d4a70758476f6469152dbd2e906672276519
Mirza Krak <mirza.krak@hostmobility.com>
2015-07-13 14:45:26 +0200

    ARM: mx4-t20: Disable wake on cd_gpio

--- arch/arm/mach-tegra/board-mx4-t20.c
+++ arch/arm/mach-tegra/board-mx4-t20.c
@@ -831,6 +831,7 @@ static struct tegra_sdhci_platform_data colibri_t20_sdhci_wifi_platform_data = {
 
 static struct tegra_sdhci_platform_data colibri_t20_sdhci_mmc_platform_data = {
 	.cd_gpio		= MMC_CD,
+	.cd_gpio_wake	= 0,
 	.is_8bit		= 0,
 	.power_gpio		= -1,
 	.wp_gpio		= -1,
