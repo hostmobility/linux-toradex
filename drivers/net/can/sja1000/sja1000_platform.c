85461f35484686ca6f7e9284a80ded93bf6a3043
Mirza Krak <mirza.krak@hostmobility.com>
2016-12-26 14:26:37 +0100

    net: can: sja1000_platform: fix 8-bit access
    MX-4 MIL board (revision < P1E) is the only board that has CAN with
    8-bit non-muxed access.
    
    Fix the sp_read/write_reg8 methods.
    
    (cherry picked from commit 63e21c3296bc3a8ca0e61f1f1a4b3b44114c4ea4)

--- drivers/net/can/sja1000/sja1000_platform.c
+++ drivers/net/can/sja1000/sja1000_platform.c
@@ -36,26 +36,29 @@ MODULE_AUTHOR("Sascha Hauer <s.hauer@pengutronix.de>");
 MODULE_DESCRIPTION("Socket-CAN driver for SJA1000 on the platform bus");
 MODULE_LICENSE("GPL v2");
 
+static DEFINE_SPINLOCK(snor_bus_lock);
+
 static u8 sp_read_reg8(const struct sja1000_priv *priv, int reg)
 {
-#if !defined(CONFIG_MACH_COLIBRI_T20) && !defined(CONFIG_MACH_COLIBRI_T30)
-	return ioread8(priv->reg_base + reg);
-#else
 	u8 value;
+	unsigned long flags;
+
+	spin_lock_irqsave(&snor_bus_lock, flags);
 	iowrite8(reg, priv->reg_base);
-	value = ioread8(priv->reg_base + 8);
+	value = ioread8(priv->reg_base + 0x20);
+	spin_unlock_irqrestore(&snor_bus_lock, flags);
+
 	return value;
-#endif
 }
 
 static void sp_write_reg8(const struct sja1000_priv *priv, int reg, u8 val)
 {
-#if !defined(CONFIG_MACH_COLIBRI_T20) && !defined(CONFIG_MACH_COLIBRI_T30)
-	iowrite8(val, priv->reg_base + reg);
-#else
+	unsigned long flags;
+
+	spin_lock_irqsave(&snor_bus_lock, flags);
 	iowrite8(reg, priv->reg_base);
-	iowrite8(val, priv->reg_base + 8);
-#endif
+	iowrite8(val, (priv->reg_base + 0x20));
+	spin_unlock_irqrestore(&snor_bus_lock, flags);
 }
 
 static u8 sp_read_reg16(const struct sja1000_priv *priv, int reg)
