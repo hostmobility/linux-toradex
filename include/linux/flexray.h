85c86a709820f15377544a589d2a1deac225c582
Tord Andersson <tord.andersson@hostmobility.com>
2017-01-27 12:41:55 +0000

    net: flexray: clean up header file
    hostmobility/mx-flexray#8

--- include/linux/flexray.h
+++ include/linux/flexray.h
@@ -26,11 +26,11 @@ typedef __u64 flexray_frame_filter_t;
    Please note that the header is not an identical
    representation of the original 5-byte header! */
 typedef struct {
-	uint32_t	flags;  /* Frame and error flags */
-	uint16_t	fid;	/* Frame id/slot id */
-	uint8_t		plr;	/* 0-254 bytes in data[] area */
-	uint16_t	crc;	/* original FlexRay header CRC */
-	uint8_t		rcc;	/* FlexRay received cycle count */
+	__u32	flags;  /* Frame and error flags */
+	__u16	fid;	/* Frame id/slot id */
+	__u8	plr;	/* 0-254 bytes in data[] area */
+	__u16	crc;	/* original FlexRay header CRC */
+	__u8	rcc;	/* FlexRay received cycle count */
 } flexray_header_t;
 
 /**
@@ -42,117 +42,14 @@ typedef struct {
 
 /* FlexRay frame with associated metadata */
 struct flexray_frame {
-	uint32_t	seq;
-	uint64_t    timestamp;  /* timeval or timespec */
+	__u32	seq;
+	__u64	timestamp;  /* timeval or timespec */
 	flexray_header_t frhead; /* slightly modified FR header */
-	uint8_t     data[254] __attribute__((aligned(8)));  //
-	uint8_t     crc[3];     /* Use TBI */
+	__u8	data[254] __attribute__((aligned(8)));  //
+	__u8	crc[3];     /* Use TBI */
 };
 
 typedef struct flexray_frame flexray_frame_t;
-
-#if 0
-// Old implementation used wrapper functions to access the bit-fields.
-static inline int flexray_get_ppi(__u8 *head)
-{
-	return !!(head[0] & FLEXRAY_PPI_FLAG);
-}
-
-static inline void flexray_set_ppi(int ppi, __u8 *head)
-{
-	if (ppi)
-		head[0] |= FLEXRAY_PPI_FLAG;
-	else
-		head[0] &= ~FLEXRAY_PPI_FLAG;
-}
-
-static inline int flexray_get_nfi(__u8 *head)
-{
-	return !!(head[0] & FLEXRAY_NFI_FLAG);
-}
-
-static inline void flexray_set_nfi(int nfi, __u8 *head)
-{
-	if (nfi)
-		head[0] |= FLEXRAY_NFI_FLAG;
-	else
-		head[0] &= ~FLEXRAY_NFI_FLAG;
-}
-
-static inline int flexray_get_syn(__u8 *head)
-{
-	return !!(head[0] & FLEXRAY_SYN_FLAG);
-}
-
-static inline void flexray_set_syn(int syn, __u8 *head)
-{
-	if (syn)
-		head[0] |= FLEXRAY_SYN_FLAG;
-	else
-		head[0] &= ~FLEXRAY_SYN_FLAG;
-}
-
-static inline int flexray_get_sfi(__u8 *head)
-{
-	return !!(head[0] & FLEXRAY_SFI_FLAG);
-}
-
-static inline void flexray_set_sfi(int sfi, __u8 *head)
-{
-	if (sfi)
-		head[0] |= FLEXRAY_SFI_FLAG;
-	else
-		head[0] &= ~FLEXRAY_SFI_FLAG;
-}
-
-static inline __u16 flexray_get_id(__u8 *head)
-{
-	return ((head[0] & 0x3) << 8) | head[1];
-}
-
-static inline void flexray_set_id(__u16 id, __u8 *head)
-{
-	head[0] &= ~0x3;
-	head[0] |= (id >> 8) & 0x3;
-	head[1] = id & 0xff;
-}
-
-static inline __u8 flexray_get_pl(__u8 *head)
-{
-	return head[2] >> 1;
-}
-
-static inline void flexray_set_pl(__u8 pl, __u8 *head)
-{
-	head[2] &= 0x1;
-	head[2] |= pl << 1;
-}
-
-static inline __u16 flexray_get_hcrc(__u8 *head)
-{
-	return ((head[2] & 0x1) << 10) | (head[3] << 2) | (head[4] >> 6);
-}
-
-static inline void flexray_set_hcrc(__u16 hcrc, __u8 *head)
-{
-	head[2] &= 0xfe;
-	head[4] &= 0x3f;
-	head[2] |= (hcrc >> 10) & 0x1;
-	head[3] = (hcrc >> 2) & 0xff;
-	head[4] |= (hcrc & 0x3) << 6;
-}
-
-static inline __u8 flexray_get_cc(__u8 *head)
-{
-	return head[4] & 0x3f;
-}
-
-static inline void flexray_set_cc(__u8 cc, __u8 *head)
-{
-	head[4] &= ~0x3f;
-	head[4] |= cc & 0x3f;
-}
-#endif
 
 /* particular protocols of the protocol family PF_FLEXRAY */
 #define FLEXRAY_RAW      1 /* RAW sockets */
@@ -170,14 +67,6 @@ static inline void flexray_set_cc(__u8 cc, __u8 *head)
 struct sockaddr_flexray {
 	sa_family_t	flexray_family;
 	int		flexray_ifindex;
-#if 0
-   union {
-      /* transport protocol class address information (e.g. ISOTP) */
-      struct { flexray_header_t rx_id, tx_id; } tp;
-
-      /* reserved for future CAN protocols address information */
-   } flexray_addr;
-#endif
 };
 
 struct flexray_filter {
