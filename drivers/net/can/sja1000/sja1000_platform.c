05efb148532f0c1e9c9437e08c7dcfd6c09ad9a5
Mirza Krak <mirza.krak@endian.se>
2018-04-18 22:08:46 +0000

    net: can: sja1000: hackis 8-bit reg access
    We have hardware that expects the 8-bit reg access
    to actually latch out the adress before the read/write
    because we do not rely on the memory controller to do it
    for us.
    
    Signed-off-by: Mirza Krak <mirza.krak@endian.se>

--- drivers/net/can/sja1000/sja1000_platform.c
+++ drivers/net/can/sja1000/sja1000_platform.c
@@ -28,6 +28,7 @@
 #include <linux/io.h>
 #include <linux/of.h>
 #include <linux/of_irq.h>
+#include <linux/spinlock_types.h>
 
 #include "sja1000.h"
 
@@ -40,14 +41,29 @@ MODULE_DESCRIPTION("Socket-CAN driver for SJA1000 on the platform bus");
 MODULE_ALIAS("platform:" DRV_NAME);
 MODULE_LICENSE("GPL v2");
 
+static DEFINE_SPINLOCK(snor_bus_lock);
+
 static u8 sp_read_reg8(const struct sja1000_priv *priv, int reg)
 {
-	return ioread8(priv->reg_base + reg);
+	u8 value;
+	unsigned long flags;
+
+	spin_lock_irqsave(&snor_bus_lock, flags);
+	iowrite8(reg, priv->reg_base);
+	value = ioread8(priv->reg_base + 0x20);
+	spin_unlock_irqrestore(&snor_bus_lock, flags);
+
+	return value;
 }
 
 static void sp_write_reg8(const struct sja1000_priv *priv, int reg, u8 val)
 {
-	iowrite8(val, priv->reg_base + reg);
+	unsigned long flags;
+
+	spin_lock_irqsave(&snor_bus_lock, flags);
+	iowrite8(reg, priv->reg_base);
+	iowrite8(val, (priv->reg_base + 0x20));
+	spin_unlock_irqrestore(&snor_bus_lock, flags);
 }
 
 static u8 sp_read_reg16(const struct sja1000_priv *priv, int reg)
