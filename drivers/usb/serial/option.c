0cafe059bc2089223e11eac564f124abc65fec35
rikardo <rikard.olander@hostmobility.com>
2019-12-11 14:38:02 +0100

    Try to add quectel modem support

--- drivers/usb/serial/option.c
+++ drivers/usb/serial/option.c
@@ -560,6 +560,16 @@ static const struct option_blacklist_info zte_mf626_blacklist = {
 };
 
 static const struct usb_device_id option_ids[] = {
+    	{ USB_DEVICE(0x05C6, 0x9090) }, /* Quectel UC15 */
+    	{ USB_DEVICE(0x05C6, 0x9003) }, /* Quectel UC20 */
+    	{ USB_DEVICE(0x2C7C, 0x0125) }, /* Quectel EC25 */
+    	{ USB_DEVICE(0x2C7C, 0x0121) }, /* Quectel EC21 */
+    	{ USB_DEVICE(0x05C6, 0x9215) }, /* Quectel EC20 */
+    	{ USB_DEVICE(0x2C7C, 0x0191) }, /* Quectel EG91 */
+    	{ USB_DEVICE(0x2C7C, 0x0195) }, /* Quectel EG95 */
+	{ USB_DEVICE(0x2C7C, 0x0306) }, /* Quectel EG06/EP06/EM06 */
+    	{ USB_DEVICE(0x2C7C, 0x0296) }, /* Quectel BG96 */
+    	{ USB_DEVICE(0x2C7C, 0x0435) }, /* Quectel AG35 */
 	{ USB_DEVICE(OPTION_VENDOR_ID, OPTION_PRODUCT_COLT) },
 	{ USB_DEVICE(OPTION_VENDOR_ID, OPTION_PRODUCT_RICOLA) },
 	{ USB_DEVICE(OPTION_VENDOR_ID, OPTION_PRODUCT_RICOLA_LIGHT) },
@@ -1211,6 +1221,7 @@ static struct usb_driver option_driver = {
 #ifdef CONFIG_PM
 	.suspend    = usb_serial_suspend,
 	.resume     = usb_serial_resume,
+	.reset_resume = usb_wwan_resume,
 	.supports_autosuspend =	1,
 #endif
 	.id_table   = option_ids,
@@ -1353,6 +1364,11 @@ static int option_probe(struct usb_serial *serial,
 		serial->dev->descriptor.idVendor == PIRELLI_VENDOR_ID) &&
 		serial->interface->cur_altsetting->desc.bInterfaceClass != 0xff)
 		return -ENODEV;
+
+    	//Quectel EC25&EC21&EG91&EG95&EG06&EP06&EM06&BG96/AG35's interface 4 can be used as USB network device
+    	if (serial->dev->descriptor.idVendor == cpu_to_le16(QUECTEL_VENDOR_ID)
+            && serial->interface->cur_altsetting->desc.bInterfaceNumber >= 4)
+	        return -ENODEV;
 
 	/* Don't bind reserved interfaces (like network ones) which often have
 	 * the same class/subclass/protocol as the serial interfaces.  Look at
